655e329f4b2cd43c666cef7c5cafcb7251f7afdd
s-silvestri <silvestri.sfn@gmail.com>
2021-10-06 17:51:28 +0200

    aggiunto il brutto, sistemato il bello

--- Spinacine.py
+++ Spinacine.py
@@ -8,6 +8,8 @@ from scipy.io import wavfile
 import wavio
 from scipy import interpolate
 import soundfile as sf
+from librosa import resample
+import scipy.signal as sps
 #Todo: fatti la CDF della distribuzione vera e genera random sulla CDF, poi se non funziona prova a crearti serie temporali di white gaussian noise (wgn) con media mu e varianza sigma, per poi trattarlo come lo tratteresti normalmente.
 file='F:/pint/Misure Coltano/pint/unzipped/run_spezzate/giro_lungo.wav'
 sampling_rate, data=wavfile.read(file)
@@ -18,6 +20,9 @@ IRF=np.array(IRF)
 #IRF[0]/=(35/durabin) #Riporto la potenza a valori contenuti in 0.2 secondi
 plt.close() #per rivederlo: plt.loglog(IRF[1], IRF[0])
 pezzobello=data[559*44100:594*44100]
+crack=data[167*44100:174*44100]
+scale=813.5330/32767
+
 
 #Da debuggare, vedi descrizione.
 def PSDToSeries(lenTimeSeries,freq,psdLoad):
@@ -28,16 +33,16 @@ def PSDToSeries(lenTimeSeries,freq,psdLoad):
     #
     #Intervallo in frequenza
     df=freq[1]-freq[0]
-    print('df = ', df)
+    #print('df = ', df)
 
     spettro0=psdLoad
     irf0=IRF[1]
     f=interpolate.interp1d(irf0, spettro0, kind='linear')
     irf1=np.linspace(IRF[1][0], IRF[1][-1], num=int(lenTimeSeries))
     spettro1=f(irf1)
-
+    binwidth=(irf1[1:]-irf1[:-1])[0]
     #Ampiezze spettrali, prese dai bin PSD: attenzione che è dilatato dall'oversampling
-    amplitude=np.sqrt(2*spettro1*df*(2049/lenTimeSeries))
+    amplitude=np.sqrt(4*spettro1*binwidth)
 
     #creo i vettori
     epsilon=np.zeros((len(amplitude)))
@@ -62,18 +67,18 @@ def genwhite (mean, std, seconds):
 def genstradabella(s):
     return(genwhite(1,5.54e-3,s))
 
-def genspettrobello(irf=IRF):
-    logmean=1.0186691440524822
-    logvar=0.1763178502675603
-    mean=-2*log(logmean)-0.5*np.log(logvar+logmean**2)
-    std=np.sqrt(-2*log(logmean)+np.log(logvar+logmean**2))
-    #std=0.42
-    spettro=np.random.lognormal(mean, std, 2049)
+# def genspettrobello(irf=IRF):
+#     logmean=1.0186691440524822
+#     logvar=0.1763178502675603
+#     mean=-2*log(logmean)-0.5*np.log(logvar+logmean**2)
+#     std=np.sqrt(-2*log(logmean)+np.log(logvar+logmean**2))
+#     #std=0.42
+#     spettro=np.random.lognormal(mean, std, 2049)
     return (spettro*IRF[0])
 
 def genspettroIRF(CDF, pdf):
     spettro=FromCDF(CDF,pdf)
-    return (spettro*IRF[0])
+    return (np.array(spettro)*IRF[0])
 
 
 # Genera il segnale atteso da una buca che dura seconds secondi, campionato a 44100 Hz
@@ -115,7 +120,7 @@ def FromCDF (CDF, pdf):
 #Inferenza: si crea una psd in un punto a caso di un intervallo bello di durata 0.2 secondi, divide per la IRF e calcola il valor medio. La sigma delle medie sarà la varianza campionaria da mertere in genstradabella. Vado da 0:93 per fermarmi a 1kHz
 avg=[]
 psdarray=[]
-for j in range (0, 100):
+for j in range (0, 1000):
     rand=np.random.random()*33
     psdbello=plottapsdsbucata(pezzobello, (rand, rand+durabin))
     psdarray.append(psdbello[0])
@@ -128,6 +133,22 @@ cdf=[]
 for j in range (0,len(pdf[0])):
     cdf.append(np.sum(pdf[0][0:j]/np.sum(pdf[0])))
 
+avgcrack=[]
+psdarraycrack=[]
+for j in range (0, 1000):
+    rand=np.random.random()*6
+    psdcrack=plottapsdsbucata(crack, (rand, rand+durabin))
+    psdarraycrack.append(psdcrack[0])
+    plt.close()
+    #avg.append(np.mean(psdbello[0]/IRF[0])) # Falsava i risultati lisciando con la media
+    avgcrack=avg+((psdcrack[0]/IRF[0]).tolist())
+pdfcrack=plt.hist(avgcrack, bins=10000)
+plt.close()
+cdfcrack=[]
+for j in range (0,len(pdf[0])):
+    cdfcrack.append(np.sum(pdfcrack[0][0:j]/np.sum(pdfcrack[0])))
+
+
 psdarray=np.array(psdarray)
 avgarray=[]
 stdarray=[]
@@ -135,27 +156,64 @@ for j in range (0, len(psdarray[1])):
     avgarray.append(np.mean(psdarray[:,j]/IRF[0][j]))
     stdarray.append(np.std(psdarray[:,j]/IRF[0][j]))
 
+
 #Crossscheck: faccio lo stesso con lo spettro sintetico
 avgsynth=[]
+avgsyntharray=[]
 psdarrasynth=[]
 for j in range  (0,1000):
-    psdarrasynth.append(genspettrobello())
+    psdarrasynth.append(genspettroIRF(cdf,pdf))
     avgsynth.append(np.mean(psdarrasynth[j][0:93]/IRF[0][0:93]))
 psdarrasynth=np.array(psdarrasynth)
 avgarraysynth=[]
 stdarraysynth=[]
+primoquartile=[]
+terzoquartile=[]
 for j in range (0, len(psdarrasynth[1])):
     avgarraysynth.append(np.mean(psdarrasynth[:,j]/IRF[0][j]))
+    primoquartile.append(np.percentile(psdarrasynth[:,j]/IRF[0][j],25))
+    terzoquartile.append(np.percentile(psdarrasynth[:,j]/IRF[0][j],75))
     stdarraysynth.append(np.std(psdarrasynth[:,j]/IRF[0][j]))
+
+def plottaquartilisintetici():
+    plt.loglog(IRF[1], avgarraysynth*IRF[0], label='median psd')
+    plt.loglog(IRF[1], primoquartile*IRF[0], label='first quartile', linestyle='--')
+    plt.loglog(IRF[1], terzoquartile*IRF[0], label='third quartile', linestyle='--')
+    plt.ylim(1e-4,3e3)
+    plt.legend()
+
+def plottaquartiliveri():
+    avgarrayvero=[]
+    primoquartile=[]
+    terzoquartile=[]
+    for j in range (0,np.shape(psdarray)[1]):
+        avgarrayvero.append(np.mean(psdarray[:,j]/IRF[0][j]))
+        primoquartile.append(np.percentile(psdarray[:,j]/IRF[0][j],25))
+        terzoquartile.append(np.percentile(psdarray[:,j]/IRF[0][j],75))
+    plt.loglog(IRF[1], avgarrayvero*IRF[0], label='median psd')
+    plt.loglog(IRF[1], primoquartile*IRF[0], label='first quartile', linestyle='--')
+    plt.loglog(IRF[1], terzoquartile*IRF[0], label='third quartile', linestyle='--')
+    plt.ylim(1e-4,3e3)
+    plt.legend()
+
 
 savedirbello='F:/Balto/'
 for j in range (0,100):
     spettro=genspettroIRF(cdf, pdf)
     psdsynth=PSDToSeries(44100*1.6, (IRF[1][0],IRF[1][-1]), spettro)
-    sf.write(savedirbello+str(j)+'.wav', psdsynth, int(44100/2))
+    #sf.write(savedirbello+str(j)+'.wav', psdsynth, int(44100/2))
+    nsamples=len(psdsynth)*2
+    sf.write(savedirbello+str(j)+'.wav', sps.resample(psdsynth, nsamples), int(44100))
+
+# savedirbrutto='F:/Brent/'
+# for j in range (0,100):
+#     spettro=genspettroIRF(cdfcrack, pdfcrack)
+#     psdsynthcrack=PSDToSeries(44100*1.6, (IRF[1][0],IRF[1][-1]), spettro)
+#     nsamples=len(psdsynthcrack)*2
+#     sf.write(savedirbrutto+str(j)+'.wav', sps.resample(psdsynthcrack, nsamples), int(44100))
 
 # for j in range (0,1000):
-#     plt.loglog(IRF[1], genspettrobello())
+#     plt.loglog(IRF[1], genspettroIRF(cdf,pdf))
 #     plt.ylim(1e-6, 2e4)
 # plt.show()
 
